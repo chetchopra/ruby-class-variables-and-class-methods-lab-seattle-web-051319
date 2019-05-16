65ac3577b69975462c96feefefae9e3162c788e9
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 00:01:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,9 +15,9 @@ class Song
     @@artists << artist
     @@genres << genre
     if genre_count.has_key?(genre)
-      genre_count[genre] += 1
+      @@genre_count[genre] += 1
     else 
-      genre_count[genre] = 1
+      @@genre_count[genre] = 1
     end
   end
   
